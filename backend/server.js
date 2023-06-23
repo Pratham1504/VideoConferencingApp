feec8028ffb429ca2f5f8cbcb35ca7baed45e86c
Raghav <raghav03.doda@gmail.com>
2023-06-23 13:09:32 +0530

    chat backend implemented

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const app = require('express')();
 
+const server = require('http').createServer();
 
+const io = require('socket.io')(server,{
+    cors : {
+        origin:"*"
+    }
+});
 
-app.listen(process.env.PORT,()=>{
-    console.log('listening to the port ' + process.env.PORT);
+io.on("connection",(socket)=>{
+    console.log("What is this socket: ",socket);
+    console.log("Socket is active to be connected");
+
+    socket.on("chat",(payload)=>{
+        console.log("What is payload",payload);
+        io.emit("chat",payload);
+    });
 })
+
+// app.listen(8000,()=>console.log("server is active...."));
+
+server.listen(process.env.PORT,()=>console.log(`server is listening at port ${process.env.PORT} ....`))
